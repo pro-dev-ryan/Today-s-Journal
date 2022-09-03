a4f61d130b242e2cd34c328f186081a99d16e960
pro-dev-ryan <azizul.developer@gmail.com>
2022-09-04 05:43:53 +0600

    design debug

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 catView = async(id)=> {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
-    const filterdData = await res.json()
-    const catArray = filterdData.data;
-    displayNews(catArray);
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+        const filterdData = await res.json()
+        const catArray = filterdData.data;
+         displayNews(catArray);     
+    } catch (error) {
+        console.log(error);
+    }
+   
+    
 }
 
 catView('02')
@@ -11,29 +17,30 @@ displayNews = async (ref) => {
     const parentDiv = document.getElementById('newsDiv');
     parentDiv.textContent = '';
     ref.forEach(news => {
+        console.log(news);
         const {author, details, image_url, others_info , thumbnail_url, title, total_view} = news;
        const singleNews = document.createElement('div');
        singleNews.innerHTML = `
        <div
        class="container mx-auto my-8 border rounded-xl border-slate-300 border-opacity-25 flex shadow-lg shadow-red-100
-        w-4/6 h-auto"
+        w-1/2 "
      >
-       <div class="img w-1/3">
+       <div class="img w-52 h-44">
          <img
-           src="${image_url ? thumbnail_url : N/A}"
-           class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-full md:rounded-none md:rounded-l-lg"
+           src="${thumbnail_url}"
+           class="object-cover w-full h-96 rounded-t-lg md:h-48 md:w-full md:rounded-none md:rounded-l-lg"
            alt=""
          />
        </div>
-       <div class="card-body w-2/3 bg-pink-50">
-         <h2 class="pl-3 mt-5 text-2xl font-semibold tracking-wide  text-rose-600">
-           ${title}
+       <div class="card-body w-full bg-pink-50">
+         <h2 class="pl-1 mt-1 text-xl tracking-tight  text-rose-600">
+           ${title?.length > 35 ? title.split(' ', 7).join(' ') : title}
          </h2>
          <p class="mr-16 ml-3 my-3 font-normal text-sm text-gray-500">
-           ${details.length > 300 ? details.slice(0, 305) + (" ...")  : details}
+           ${details.length > 300 ? details.slice(0, 150) + (" ...")  : details}
          </p>
          <div
-           class="card-footer mt-8 px-5 py-1 flex justify-between items-center bg-teal-100 bg-opacity-30"
+           class="card-footer h-12 px-1 my-5 flex justify-between items-center bg-teal-100 bg-opacity-30"
          >
            <div class="viewDiv flex">
              <svg
@@ -42,7 +49,7 @@ displayNews = async (ref) => {
                viewBox="0 0 24 24"
                stroke-width="1.5"
                stroke="currentColor"
-               class="w-6 h-6"
+               class="w-4 h-4"
              >
                <path
                  stroke-linecap="round"
@@ -55,7 +62,7 @@ displayNews = async (ref) => {
                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                />
              </svg>
-             <p class="pl-2">${total_view} views</p>
+             <p class="text-xs pl-1">${total_view? total_view +" views" : 'N/A'  }</p>
            </div>
            <button class="relative inline-flex items-center justify-center  text-sm font-medium text-emerald-600 hover:text-red-500">Read More<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                   <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
@@ -66,12 +73,12 @@ displayNews = async (ref) => {
            <div class="userDiv flex">
              <img
                src="${author.img? author.img : 'N/A'}"
-               class="w-12 rounded-full"
+               class=" h-10 rounded-full"
                alt=""
              />
              <div class="user ml-2">
-               <p class="text-sm font-medium">${author.name? author.name : 'unknown'}</p>
-               <p class="text-sm font-light">${author.published_data? author.published_data : 'N/A' }</p>
+               <p class="text-xs font-medium">${author?.name? author.name : 'anonymous'}</p>
+               <p class="text-xs font-light">${author.published_date? author.published_date : 'N/A' }</p>
              </div>
            </div>
          </div>
